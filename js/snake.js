c21e2923c98246cc79e6c0847cc8ee98a1676ec5
Villy <villyjord@yahoo.com>
2016-01-27 23:41:10 +0200

    Starting the scoring

--- js/snake.js
+++ js/snake.js
@@ -8,8 +8,9 @@ var height = canvas.height;
 
 // Work out the width and height in blocks
 var blockSize = 10;
-//var widthInBlocks = width / blockSize;
-//var heightInBlocks = height / blockSize;
+
+// Score count
+var score = 0;
 
 
 // The Block constructor
@@ -24,6 +25,15 @@ Block.prototype.drawSquare = function (color) {
     var y = this.row * blockSize;
     ctx.fillStyle = color;
     ctx.fillRect(x, y, blockSize, blockSize);
+};
+
+// Draw the score in the top-left corner
+var drawScore = function () {
+    ctx.font = "10px Courier";
+    ctx.fillStyle = "Black";
+    ctx.textAlign = "left";
+    ctx.textBaseline = "top";
+    ctx.fillText("Score: " + score, blockSize, blockSize);
 };
 
 var Food = function() {
